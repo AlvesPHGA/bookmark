76873f147c08a009ef103a9af819052001601107
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-16 14:45:35 -0300

    feat: create NavComponent

--- src/ui/partials/Global/Header.tsx
+++ src/ui/partials/Global/Header.tsx
@@ -1,9 +1,14 @@
+import { NavComponent } from '../../components/NavComponent';
 import { Logo } from '../../components/SVGs/Logo';
+import { SectionComponent } from '../../components/TagsComponents';
 
 export const Header = () => {
    return (
       <header>
-         <Logo />
+         <SectionComponent>
+            <Logo />
+            <NavComponent />
+         </SectionComponent>
       </header>
    );
 };
