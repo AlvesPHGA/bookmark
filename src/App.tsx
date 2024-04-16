49dad89287c8fa420fd41f8691db0fed5f34af23
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-16 14:23:54 -0300

    update: add Home component on app doc

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Home } from './pages/Home';
 import { Header } from './ui/partials/Global/Header';
 
 function App() {
    return (
       <>
          <Header />
+         <Home />
       </>
    );
 }
