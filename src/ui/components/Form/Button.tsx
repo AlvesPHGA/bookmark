7bdec2c6c7e045cd03dbfcab626b385814cffb4c
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-17 09:53:37 -0300

    feat: create IForms to form's components'

--- src/ui/components/Form/Button.tsx
+++ src/ui/components/Form/Button.tsx
@@ -1,3 +1,10 @@
-export const Button = ({ ...props }) => {
-   return <button {...props} />;
+import { ButtonProps } from '../../../data/@types/IForms';
+
+export const Button = ({ ...props }: ButtonProps) => {
+   return (
+      <button
+         className="uppercase border-2 border-softRed rounded-md px-7 py-1 text-white bg-softRed hover:text-softRed hover:bg-transparent transition-all"
+         {...props}
+      />
+   );
 };
