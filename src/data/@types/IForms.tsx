09a7294b87ea70605d4498888506e3613009487a
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-17 09:58:32 -0300

    update: header

--- src/data/@types/IForms.tsx
+++ src/data/@types/IForms.tsx
@@ -1,3 +1,7 @@
 import React from 'react';
+import { VariantProps } from 'tailwind-variants';
+import { buttonStyle } from '../../ui/styles/formsStyles';
 
-export interface ButtonProps extends React.ComponentProps<'button'> {}
+export interface ButtonProps
+   extends React.ComponentProps<'button'>,
+      VariantProps<typeof buttonStyle> {}
--- src/ui/components/Form/Button.tsx
+++ src/ui/components/Form/Button.tsx
@@ -1,10 +1,6 @@
 import { ButtonProps } from '../../../data/@types/IForms';
+import { buttonStyle } from '../../styles/formsStyles';
 
 export const Button = ({ ...props }: ButtonProps) => {
-   return (
-      <button
-         className="uppercase border-2 border-softRed rounded-md px-7 py-1 text-white bg-softRed hover:text-softRed hover:bg-transparent transition-all"
-         {...props}
-      />
-   );
+   return <button className={buttonStyle()} {...props} />;
 };
