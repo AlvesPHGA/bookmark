e3e9cd4e72781c2a7ff0196a222dc834f87d6116
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-17 15:52:57 -0300

    feat: create ContentSBMComponent

---
+++ src/ui/components/SBM/ContentSBMComponent.tsx
@@ -0,0 +1,15 @@
+import { DivComponent } from '../TagsComponents';
+
+export const ContentSBMComponent = () => {
+   return (
+      <DivComponent>
+         <h1>A Simple Bookmark Manager</h1>
+         <p>
+            A clean and simple interface to organize your favourite websites.
+            Open a new browser tab and see your sites load instantly. Try it for
+            free.
+         </p>
+         <DivComponent></DivComponent>
+      </DivComponent>
+   );
+};
