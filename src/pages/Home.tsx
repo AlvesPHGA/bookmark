0e60d7e6ca6315efa3d3fe4998f0dc283a9130a2
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-18 16:14:36 -0300

    feat: create a partial with name Features

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,11 @@
+import { Features } from '../ui/partials/Features';
 import { SimpleBookmarkManager } from '../ui/partials/SimpleBookmarkManager';
 
 export const Home = () => {
    return (
       <main>
          <SimpleBookmarkManager />
+         <Features />
       </main>
    );
 };
