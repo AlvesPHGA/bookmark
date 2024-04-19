f7b22bb632d32e9fc020b691905296f83d0df43d
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-19 09:20:08 -0300

    feat: create ContentTabComponent

--- src/ui/components/Features/ContentTabsComponent.tsx
+++ src/ui/components/Features/ContentTabsComponent.tsx
@@ -1,5 +1,10 @@
+import { Illustration_tab1 } from '../SVGs/Illustration';
 import { DivComponent } from '../TagsComponents';
 
 export const ContentTabsComponent = () => {
-   return <DivComponent></DivComponent>;
+   return (
+      <DivComponent>
+         <Illustration_tab1 />
+      </DivComponent>
+   );
 };
