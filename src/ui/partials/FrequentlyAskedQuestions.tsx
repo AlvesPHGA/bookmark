6db199a624208e2839aaf3f02842eb4698d2311b
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-22 14:14:55 -0300

    update: add FAQContentComponent on FrequentlyAskedQuestions

--- src/ui/partials/FrequentlyAskedQuestions.tsx
+++ src/ui/partials/FrequentlyAskedQuestions.tsx
@@ -1,5 +1,10 @@
+import { FAQContentComponent } from '../components/FAQ/FAQContentComponent';
 import { SectionComponent } from '../components/TagsComponents';
 
 export const FrequentlyAskedQuestions = () => {
-   return <SectionComponent></SectionComponent>;
+   return (
+      <SectionComponent>
+         <FAQContentComponent />
+      </SectionComponent>
+   );
 };
