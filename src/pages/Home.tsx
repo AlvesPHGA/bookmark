035fc80789863d6a654980dc66fa8e0c5029a2ad
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-22 14:09:15 -0300

    update: add FrequentlyAskedQuestions on home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { Download } from '../ui/partials/Download';
 import { Features } from '../ui/partials/Features';
+import { FrequentlyAskedQuestions } from '../ui/partials/FrequentlyAskedQuestions';
 import { SimpleBookmarkManager } from '../ui/partials/SimpleBookmarkManager';
 
 export const Home = () => {
@@ -8,6 +9,7 @@ export const Home = () => {
          <SimpleBookmarkManager />
          <Features />
          <Download />
+         <FrequentlyAskedQuestions />
       </main>
    );
 };
