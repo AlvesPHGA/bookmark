c86432738aea759d3078a7f0bf5d35eecfae2886
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-22 09:07:17 -0300

    feat: create DownloadContentComponent

---
+++ src/ui/components/Download/DownloadContentComponent.tsx
@@ -0,0 +1,13 @@
+import { DivComponent } from '../TagsComponents';
+
+export const DownloadContentComponent = () => {
+   return (
+      <DivComponent>
+         <h2>Download the extension</h2>
+         <p>
+            We've got more browsers in the pipeline. Please do let us know if
+            you've got a favourite you'd like us to prioritize.
+         </p>
+      </DivComponent>
+   );
+};
