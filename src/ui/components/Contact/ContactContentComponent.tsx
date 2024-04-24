ad2c2e28c924ae765298a629df2e085a100aa936
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-24 10:50:30 -0300

    feat: create ContactContentComponent

---
+++ src/ui/components/Contact/ContactContentComponent.tsx
@@ -0,0 +1,10 @@
+import { DivComponent } from '../TagsComponents';
+
+export const ContactContentComponent = () => {
+   return (
+      <DivComponent>
+         <span>35,000+ ALREADY JOINED</span>
+         <h2>Stay up-to-date with what we're doing</h2>
+      </DivComponent>
+   );
+};
