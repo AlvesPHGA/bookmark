e9a582996128e2202ca270c1b321225c8553dea3
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-24 11:34:39 -0300

    feat: add Input and Button on ContactFormComponent

--- src/ui/components/Contact/ContactFormComponent.tsx
+++ src/ui/components/Contact/ContactFormComponent.tsx
@@ -1,3 +1,11 @@
+import { Button } from '../Form/Button';
+import { Input } from '../Form/Input';
+
 export const ContactFormComponent = () => {
-   return <form></form>;
+   return (
+      <form>
+         <Input />
+         <Button>Contact Us</Button>
+      </form>
+   );
 };
