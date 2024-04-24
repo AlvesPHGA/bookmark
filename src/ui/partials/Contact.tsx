12eccf7f87e7af39baf99fda820bc955eb88d578
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-24 11:36:10 -0300

    feat: add ContactFormComponent on Contact

--- src/ui/partials/Contact.tsx
+++ src/ui/partials/Contact.tsx
@@ -1,10 +1,12 @@
 import { ContactContentComponent } from '../components/Contact/ContactContentComponent';
+import { ContactFormComponent } from '../components/Contact/ContactFormComponent';
 import { SectionComponent } from '../components/TagsComponents';
 
 export const Contact = () => {
    return (
       <SectionComponent section="contact">
          <ContactContentComponent />
+         <ContactFormComponent />
       </SectionComponent>
    );
 };
