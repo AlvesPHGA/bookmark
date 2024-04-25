8b09a8efaec55915d13d7860185f05b0e0f1843b
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-04-25 12:01:34 -0300

    feat: create a new function to valid contact form

--- src/ui/components/Contact/ContactFormComponent.tsx
+++ src/ui/components/Contact/ContactFormComponent.tsx
@@ -4,16 +4,31 @@ import { Input } from '../Form/Input';
 import { DivComponent } from '../TagsComponents';
 
 export const ContactFormComponent = () => {
-   const [name, setName] = React.useState('');
+   const [email, setEmail] = React.useState('');
 
-   function handleChange(ev) {
-      console.log(ev.target.value);
+   function handleSubmitContact(ev) {
+      const regEmail = /^[a-z0–9._-]+@[a-z0–9.-]+\.[a-z]{2,4}$/;
+
+      ev.preventDefault();
+
+      if (email === '' || email.length < 3) {
+         alert('Preencher campo email');
+      } else {
+         if (!regEmail.test(email)) {
+            alert('Email invalido');
+         } else {
+            alert('Email valido');
+         }
+      }
    }
 
    return (
-      <form className="flex w-fit mx-auto space-x-2">
+      <form
+         onSubmit={handleSubmitContact}
+         className="flex w-fit mx-auto space-x-2"
+      >
          <DivComponent>
-            <Input onChange={handleChange} />
+            <Input onChange={(ev) => setEmail(ev.currentTarget.value)} />
          </DivComponent>
          <Button types="btn_contact">Contact Us</Button>
       </form>
