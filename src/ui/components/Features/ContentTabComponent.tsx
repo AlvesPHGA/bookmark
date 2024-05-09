fbcdd36f3a85cab24f6e742a807216fa81e8d744
Pedro H <pedrohenriquegomesalves@gmail.com>
2024-05-09 12:01:13 -0300

    style: Feature section

--- src/ui/components/Features/ContentTabComponent.tsx
+++ src/ui/components/Features/ContentTabComponent.tsx
@@ -1,15 +1,19 @@
 import { ContentTabComponentProps } from '../../../data/@types/ITab';
-import { DivComponent, NavLinkComponent } from '../TagsComponents';
+import { NavLinkComponent } from '../TagsComponents';
+
+import { featuresStyle } from '../../styles/featuresStyle';
+
+const { tab_content, cta } = featuresStyle();
 
 export const ContentTabComponent = ({
    title,
    text,
 }: ContentTabComponentProps) => {
    return (
-      <DivComponent box="tab_content">
+      <div className={tab_content()}>
          <h3 className="text-3xl font-medium">{title}</h3>
          <p>{text}</p>
-         <NavLinkComponent link="blue_cta">More Info</NavLinkComponent>
-      </DivComponent>
+         <NavLinkComponent className={cta()}>More Info</NavLinkComponent>
+      </div>
    );
 };
--- src/ui/styles/featuresStyle.tsx
+++ src/ui/styles/featuresStyle.tsx
@@ -9,6 +9,7 @@ export const featuresStyle = tv({
       tab_contents:
          'hidden w-full h-fit  items-center justify-between pr-14 z-10',
       tab_content: 'relative space-y-7 text-left w-[420px]',
+      cta: 'transition bg-[#5267DF] py-2.5 px-6 rounded text-white font-medium border-2 border-[#5267DF] hover:text-[#5267DF] hover:bg-transparent block w-fit',
    },
 
    variants: {
